e67e50b8537cd14e6f1007c52da818ed421730b8
黄勇 <yong.huang@dianrong.com>
2016-10-14 10:44:54 +0800

    jason转换验证

--- src/test/java/com/auto/TestHttp.java
+++ src/test/java/com/auto/TestHttp.java
@@ -13,9 +13,20 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.testng.annotations.Test;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
+
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.JsonNodeFactory;
+
+
+
+
 
 public class TestHttp {
      @Test
@@ -27,6 +38,19 @@ public class TestHttp {
 		        HttpEntity entity = response.getEntity();  
 		        String response_str =  EntityUtils.toString(entity, "utf-8");
 		        EntityUtils.consume(entity);
+		        
+		        try {
+		        	 ObjectMapper mapper = new ObjectMapper(); 
+		        	 Map m = mapper.readValue(response_str, Map.class);
+		        	 System.out.println(m.get("content"));
+		        }
+		        catch (JsonParseException e) {
+		            e.printStackTrace();
+		        } catch (JsonMappingException e) {
+		            e.printStackTrace();
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
 
 		    }
 		    catch(Exception e){
