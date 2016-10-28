0d87879b710698ac9aa5e0f6e3ff1cd40f02e810
黄勇 <yong.huang@dianrong.com>
2016-10-28 17:54:25 +0800

    添加http Entity()，发送请求

--- src/main/java/com/auto/HttpHandle.java
+++ src/main/java/com/auto/HttpHandle.java
@@ -1,17 +1,29 @@
 package com.auto;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import  org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.URIBuilder;
+
+
 
 public class HttpHandle {
 	 public CloseableHttpClient httpclient; 
@@ -20,6 +32,9 @@ public class HttpHandle {
 	 public 	String response_str;
 	 public 	Map response_map;
 	 public HttpEntity repnse_entity;
+	 public HttpRequestBase request;
+	 private EntityBuilder builder; //Post, put请求的参数
+	 private URIBuilder uriBuilder; //get, delete请求的参数
 	 
 	 
 	 public HttpHandle(){
@@ -28,11 +43,14 @@ public class HttpHandle {
 	     this.response_str = null;
 	     this.repnse_entity = null;
 	     this.response_map = null;
+	     this.request = null;
+	     this.builder = EntityBuilder.create().setParameters(new ArrayList<NameValuePair>());
 	 }
+	 
 	public  Map do_get(String url){
-		this.httpGet = new HttpGet(url);
+		this.request = new HttpGet(url);
 		try {
-		this.reponse = this.httpclient.execute(this.httpGet);
+		this.reponse = this.httpclient.execute(this.request);
 		this.repnse_entity = this.reponse.getEntity();  
         this.response_str =  EntityUtils.toString(this.repnse_entity, "utf-8");
         EntityUtils.consume(this.repnse_entity);
@@ -51,6 +69,23 @@ public class HttpHandle {
         
 		}
 		catch (Exception e){
+			e.printStackTrace();
+		}
+		return this.response_map;
+	}
+	
+	public  Map do_post(String url){
+		this.request = new HttpPost(url);
+		try{
+			//this.reponse = this.httpclient.execute(this.request);
+			this.request.setHeader("Content-Type", "application/json");
+	        List params=new ArrayList();
+	        params.add(new BasicNameValuePair("account", "yong.huang@dianrong.com"));
+//	        HttpClientContext context = HttpClientContext.create();  
+//	        this.request.setEntity(new UrlEncodedFormEntity(params));
+//			
+		}
+		catch(Exception e){
 			e.printStackTrace();
 		}
 		return this.response_map;
--- src/test/java/com/auto/TestHttp.java
+++ src/test/java/com/auto/TestHttp.java
@@ -3,14 +3,17 @@ package com.auto;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.HttpEntity;  
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.util.EntityUtils;  
-import org.apache.http.message.BasicNameValuePair; 
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.entity.EntityBuilder;
 import org.apache.http.client.entity.UrlEncodedFormEntity;  
 import org.apache.http.protocol.HTTP;
 
@@ -40,36 +43,9 @@ public class TestHttp {
      @Test
 	 public void test_get_http(){
     	  HttpHandle httpHandle =  new HttpHandle();
-    	 String url = "https://drac-demo.dianrong.com/adminconsole/api/actor/11335844/investSummary";
+    	  String url = "https://drac-demo.dianrong.com/adminconsole/api/actor/11335844/investSummary";
     	  Map respnse_map = httpHandle.do_get(url);
-    	  System.out.println(respnse_map);
-//		  CloseableHttpClient httpclient = HttpClients.createDefault();
-//		    HttpGet httpGet = new HttpGet("https://drac-demo.dianrong.com/adminconsole/api/actor/11335844/investSummary");
-//		    try{
-//		        CloseableHttpResponse response = httpclient.execute(httpGet);
-//		        HttpEntity entity = response.getEntity();  
-//		        String response_str =  EntityUtils.toString(entity, "utf-8");
-//		        EntityUtils.consume(entity);
-//		        
-//		        try {
-//		        	 ObjectMapper mapper = new ObjectMapper(); 
-//		        	 Map m = mapper.readValue(response_str, Map.class);
-//		        	 System.out.println(m.get("content"));
-//		        	 
-//		        }
-//		        catch (JsonParseException e) {
-//		            e.printStackTrace();
-//		        } catch (JsonMappingException e) {
-//		            e.printStackTrace();
-//		        } catch (IOException e) {
-//		            e.printStackTrace();
-//		        }
-//
-//		    }
-//		    catch(Exception e){
-//		    	e.printStackTrace();
-//		    }
-//		    
+    	  System.out.println(respnse_map);    
 	 }
      
      @Test
@@ -77,12 +53,16 @@ public class TestHttp {
     	 	CloseableHttpClient httpclient = HttpClients.createDefault();
     	 	HttpPost httpPost = new HttpPost("http://uniauth-demo.sl.com/ws/rs/user/usedetailinfo");
 		    try{
-		    	httpPost.setHeader("Content-Type", "application/json");
 		        List params=new ArrayList();
 		        params.add(new BasicNameValuePair("account", "yong.huang@dianrong.com"));
-		        httpPost.setEntity(new UrlEncodedFormEntity(params));
-//		    	httpPost.setEntity(new StringEntity("{ \"account\": \"yong.huang@dianrong.com\"}"));
-		    	
+		        EntityBuilder entity_post =  EntityBuilder.create().setParameters(new ArrayList<NameValuePair>());
+		        entity_post.setContentType(ContentType.APPLICATION_JSON);
+//		        entity_post.setParameters(new BasicNameValuePair("account", "yong.huang@dianrong.com"));
+		        entity_post.setText("{ \"account\": \"yong.huang@dianrong.com\"}");
+		      
+		        httpPost.setEntity(entity_post.build());
+		        
+		        
 		        CloseableHttpResponse response = httpclient.execute(httpPost);
 		        HttpEntity entity = response.getEntity();  
 		        String response_str =  EntityUtils.toString(entity, "utf-8");
@@ -96,6 +76,34 @@ public class TestHttp {
 		        	
 		        }
      }
+     
+     
+     @Test
+     public void test_post1(){
+    	 	CloseableHttpClient httpclient = HttpClients.createDefault();
+    	 	HttpPost httpPost = new HttpPost("http://uniauth-demo.sl.com/ws/rs/user/usedetailinfo");
+		    try{
+		    	httpPost.setHeader("Content-Type", "application/json");
+//		        List params=new ArrayList();
+//		        params.add(new BasicNameValuePair("account", "yong.huang@dianrong.com"));
+               httpPost.setEntity(new StringEntity("{ \"account\": \"yong.huang@dianrong.com\"}"));
+		
+		        
+		        CloseableHttpResponse response = httpclient.execute(httpPost);
+		        HttpEntity entity = response.getEntity();  
+		        String response_str =  EntityUtils.toString(entity, "utf-8");
+		        System.out.print(response.getStatusLine());
+		        System.out.println(response_str);
+		        EntityUtils.consume(entity);
+		     
+		    }
+		   catch(Exception e){
+			   e.printStackTrace();
+		        	
+		        }
+     }
+     
+     
      
      @Test
      public void  test_httpUtil(){
@@ -118,7 +126,7 @@ public class TestHttp {
     	 HttpUtils http = HttpUtils.post(url);
     	// http.addParameter("wd", "java 核心技术"); //搜索关键字
     	// http.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0");
-        http.addHeader("Content-Typ", "application/json");
+        http.addHeader("Content-Type", "application/json");
         String json = "{ \"account\": \"yong.huang@dianrong.com\"}";
         ObjectMapper mapper = new ObjectMapper();  
      //   Map json_map =  mapper.readValue(json, Map.class);  
